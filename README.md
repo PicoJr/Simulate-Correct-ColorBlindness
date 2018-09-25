1050bd402e1755ec5ba439bcbebc7f06bd8a22ab
PicoJr <PicoJr@notanemail.com>
2018-09-25 21:19:23 +0200

    Refactor transformations
    remove 'correct' transformation
    remove 'normalise'
    PEP
    Update README
    Add examples

--- README.md
+++ README.md
@@ -1,44 +1,64 @@
-# Simulate-Correct-ColorBlindness
-This Script converts images to simulate Colour blindness using Daltonization Algorithm. The converted image is what is perceived by colour blind people. It also converts the image to help colour blind people differentiate between certain colours in an Image, using and HSV-Shifting Algorithm.
+# Simulate Color Blindness
 
-### Libraries Required:
-PIL, colorsys, numpy
-```shell
-sudo pip install Pillow numpy
+This is a fork of <https://github.com/tsarjak/Simulate-Correct-ColorBlindness>
+
+## This fork
+
+- Alpha channel (transparency) is supported
+- fix rotation issue
+
+The _color blindness correction_ feature has been **removed**.
+
+> I couldn't manage to make it work correctly (transparency + no picture rotation) -_-'
+
+## Install
+
 ```
-### To Install
-```shell
-git clone https://github.com/tsarjak/Simulate-Correct-ColorBlindness.git
-```
-### To run the code
-In terminal:
-```shell
-#In Home Directory or the Directory in which you cloned/downloaded/installed the script
-
-python transform.py -input <input file name> -output <output file name> -sp
-
-python transform.py --help #For Help from terminal
-
-#-sp to Simulate Protanopia
-#-sd to Simulate Deutranopia
-#-st to Simulate Tritanopia
-#-correct to correct the image for protanopia patients
+pip install -r requirements
 ```
 
-### Examples
+## Usage
 
-#### Simulating Protanopia
+```
+usage: transform.py [-h] [-input INPUT] [-output OUTPUT] (-sp | -sd | -st)
 
-Original Image : ![plate15](https://cloud.githubusercontent.com/assets/9898343/23335798/0750036a-fbe3-11e6-9295-15ea03c8429c.jpg) 
+Simulate Color Blindness
 
-Simulated Image: 
-![plate15sim](https://cloud.githubusercontent.com/assets/9898343/23335813/5509b8b2-fbe3-11e6-8fd5-fc93016e7542.jpg) (How protanopes will percieve the image!) :
+optional arguments:
+  -h, --help      show this help message and exit
+  -input INPUT    Path to input image
+  -output OUTPUT  Path for output image
+  -sp             Simulate Protanopia (Common Red-Green Blindness)
+  -sd             Simulate Deutranopia (Rare Red-Green Blindness)
+  -st             Simulate Tritanopia (Blue-Yellow Color Blindness)
+```
 
-#### Correcting Protanopia
+## Examples
 
-Original Image and How Protanopes will Perceive it : ![before_1](https://cloud.githubusercontent.com/assets/9898343/23335851/f23b4682-fbe3-11e6-9fea-2f313c6c6dd3.png)
+### original
 
-Correcting the above original image and then Simulating it for Protanopes. The number which were unreadable for protanopes have now become clear enough to be recognised! : ![comb26022017053044](https://cloud.githubusercontent.com/assets/9898343/23335899/db0687f0-fbe4-11e6-9d92-f60bf15a5ff1.jpg)
+![original](res/picoJr.png)
 
+### Protanopia
 
-_Currently Corrects only Protanopia Color Blindness via transformation. Deutranopia and Tritanopia to be added soon!_
+```
+python transform.py -sp -input res/picoJr.png -output res/picoJr-sp.png
+```
+
+![protanopia](res/picoJr-sp.png)
+
+### Deutranopia
+
+```
+python transform.py -sd -input res/picoJr.png -output res/picoJr-sd.png
+```
+
+![deutranopia](res/picoJr-sd.png)
+
+### Tritanopia
+
+```
+python transform.py -st -input res/picoJr.png -output res/picoJr-st.png
+```
+
+![tritanopia](res/picoJr-st.png)
--- transform.py
+++ transform.py
@@ -1,186 +1,96 @@
-from PIL import Image
-import numpy as np
-import numpy
 import argparse
-import sys
-import colorsys
 
-simType = 0
+import numpy
+import numpy as np
+from PIL import Image
 
-#ArgParse Function
+
 def parse_args():
-	parser = argparse.ArgumentParser(
-		description = 'Simulate and Correct Images for Color Blindness')
-	parser.add_argument(
-		'-input', type=str, help = 'Path to input image')
-	parser.add_argument(
-		'-output', type=str, help = 'Path for output image')
-	parser.add_argument('-sp', action='store_true', help = 'Simulate Protanopia (Common Red-Green  Blindness)')
-	parser.add_argument('-sd', action='store_true', help = 'Simulate Deutranopia (Rare Red-Green Blindness)')
-	parser.add_argument('-st', action='store_true', help = 'Simulate Tritanopia (Blue-Yellow Color Blindness)')
-	parser.add_argument('-correct', action='store_true', help = 'Correct Image for Protanopia')
-	args = parser.parse_args()
-	return args
+    parser = argparse.ArgumentParser(
+        description='Simulate Color Blindness')
+    parser.add_argument(
+        '-input', type=str, help='Path to input image')
+    parser.add_argument(
+        '-output', type=str, help='Path for output image')
+    transform_group = parser.add_mutually_exclusive_group(required=True)
+    transform_group.add_argument('-sp', action='store_true', help='Simulate Protanopia (Common Red-Green  Blindness)')
+    transform_group.add_argument('-sd', action='store_true', help='Simulate Deutranopia (Rare Red-Green Blindness)')
+    transform_group.add_argument('-st', action='store_true', help='Simulate Tritanopia (Blue-Yellow Color Blindness)')
+    args = parser.parse_args()
+    return args
 
-#Restructuring laterally inverted image
-def normalise(editablePhoto,sizeX,sizeY):
-	NormalPhoto =  np.zeros((sizeX,sizeY,3),'float')
-	x=sizeX-1
-	y=sizeY
-	for i in range(0,sizeX):
-		for j in range(0,sizeY):
-			for k in range(0,3):
-				NormalPhoto[x,j,k]=editablePhoto[i,j,k]
-		x=x-1
 
-	return NormalPhoto
+# Matrix Multiplication Block (Common for all operations, just varying matrix)
+def get_image_array(transform_matrix, image_array):
+    width, height, _ = image_array.shape
+    for i in range(width):
+        for j in range(height):
+            curr_matrix = np.array((0, 0, 0), dtype=float)
+            for k in range(3):
+                curr_matrix[k] = image_array[i, j, k]
+            lms_image = np.dot(transform_matrix, curr_matrix)
+            for k in range(3):
+                image_array[i, j, k] = lms_image[k]
+    return image_array
 
-#Matrix Multiplication Block (Common for all operations, just varying matrix)
-def getImageArray(respectiveArray, editablePhoto, sizeX, sizeY):
-	for i in range(0,sizeX):
-		for j in range(0,sizeY):
-			currMatrix = np.array((0,0,0),dtype=float)
-			for k in range(0,3):
-				currMatrix[k]=editablePhoto[i,j,k]
-			lmsImage = np.dot(respectiveArray,currMatrix)
-			for k in range(0,3):
-				editablePhoto[i,j,k]=lmsImage[k]
-	return editablePhoto
 
-#Converting RGB to LMS
-def convertToLMS(im,sizeX,sizeY):
-	photo = im.load()
-	editablePhoto = np.zeros((sizeX,sizeY,3),'float')
-	for i in range(0,sizeX):
-		for j in range(0,sizeY):
-			for k in range(0,3):
-				editablePhoto[i,j,k] = photo[i,j][k]
-				editablePhoto[i,j,k] = ((editablePhoto[i,j,k])/255)
+# Converting RGB to LMS
+# https://en.wikipedia.org/wiki/LMS_color_space
+def convert_to_lms(input_image):
+    image_array = np.array(np.asarray(input_image))  # np.asarray is readonly
+    image_array = np.divide(image_array, 255.0)
+    lms_convert = numpy.array(
+        [[17.8824, 43.5161, 4.11935], [3.45565, 27.1554, 3.86714], [0.0299566, 0.184309, 1.46709]])
+    return get_image_array(lms_convert, image_array)
 
-	lmsConvert = numpy.array([[17.8824,43.5161,4.11935],[3.45565,27.1554,3.86714],[0.0299566,0.184309,1.46709]])
-	editablePhoto = getImageArray(lmsConvert, editablePhoto, sizeX, sizeY)
 
-	NormalPhoto =  normalise(editablePhoto,sizeX,sizeY)
-	return NormalPhoto
+# Simulating Protanopia
+def convert_to_protanopes(image):
+    protanope_convert = numpy.array([[0, 2.02344, -2.52581], [0, 1, 0], [0, 0, 1]])
+    return get_image_array(protanope_convert, image)
 
-#Simulating Protanopia
-def ConvertToProtanopes(editablePhoto,sizeX,sizeY):
-	protanopeConvert = numpy.array([[0,2.02344,-2.52581],[0,1,0],[0,0,1]])
-	editablePhoto = getImageArray(protanopeConvert, editablePhoto, sizeX, sizeY)
-	NormalPhoto = normalise(editablePhoto, sizeX, sizeY)
-	return NormalPhoto
 
-#Simulating Deutranopia
-def ConvertToDeuteranopes(editablePhoto,sizeX,sizeY):
-	DeuteranopesConvert = numpy.array([[1,0,0],[0.494207,0,1.24827],[0,0,1]])
-	editablePhoto = getImageArray(DeuteranopesConvert, editablePhoto, sizeX, sizeY)
-	NormalPhoto = normalise(editablePhoto, sizeX, sizeY)
-	return NormalPhoto
+# Simulating Deutranopia
+def convert_to_deuteranopes(image):
+    deuteranopes_convert = numpy.array([[1, 0, 0], [0.494207, 0, 1.24827], [0, 0, 1]])
+    return get_image_array(deuteranopes_convert, image)
 
-#Simulating Tritanopia
-def ConvertToTritanope(editablePhoto,sizeX,sizeY):
-	TritanopeConvert = numpy.array([[1,0,0],[0,1,0],[-0.395913,0.801109,0]])
-	editablePhoto = getImageArray(TritanopeConvert, editablePhoto, sizeX, sizeY)
-	NormalPhoto = normalise(editablePhoto, sizeX, sizeY)
-	return NormalPhoto
 
-#Converting LMS to RGB
-def convertToRGB(editablePhoto,sizeX,sizeY):
-	rgb2lms = numpy.array([[17.8824,43.5161,4.11935],[3.45565,27.1554,3.86714],[0.0299566,0.184309,1.46709]])
-	RGBConvert = numpy.linalg.inv(rgb2lms)
-	editablePhoto = getImageArray(RGBConvert, editablePhoto, sizeX, sizeY)
-	for i in range(0,sizeX):
-		for j in range(0,sizeY):
-			for k in range(0,3):
-				editablePhoto[i,j,k]=((editablePhoto[i,j,k]))*255
+# Simulating Tritanopia
+def convert_to_tritanope(image):
+    tritanope_convert = numpy.array([[1, 0, 0], [0, 1, 0], [-0.395913, 0.801109, 0]])
+    return get_image_array(tritanope_convert, image)
 
-	NormalPhoto = normalise(editablePhoto, sizeX, sizeY)
-	return NormalPhoto
 
-#Converting Processed Array to Image
-def arrayToImage(editablePhoto,sizeX,sizeY,saveAs):
-	rgbArray = np.zeros((sizeX,sizeY,3),'uint8')
-	for i in range(0,sizeX):
-		for j in range(0,sizeY):
-			for k in range(0,3):
-				rgbArray[i,j,k] = editablePhoto[i,j,k]
-	img = Image.fromarray(rgbArray)
-	img.save(saveAs)
+# Converting LMS to RGB
+def convert_to_rgb(image_array):
+    rgb2lms = numpy.array([[17.8824, 43.5161, 4.11935], [3.45565, 27.1554, 3.86714], [0.0299566, 0.184309, 1.46709]])
+    rgb_convert = numpy.linalg.inv(rgb2lms)
+    image_array_rgb = get_image_array(rgb_convert, image_array)
+    image_array_rgb = np.multiply(image_array_rgb, 255)
+    return image_array_rgb
 
-#Correcting the image using HSV Shifting Algorithm
-def correct(inputIm, sizeX,sizeY,saveAs):
 
-	photo = inputIm.load()
-	editablePhoto = np.zeros((sizeX,sizeY,3),'float')
-	hsvArray=np.zeros((sizeX,sizeY,3),'float')
-				
-	for i in range(0,sizeX):
-		for j in range(0,sizeY):
-			for k in range(0,3):
-				editablePhoto[i,j,k] = photo[i,j][k]
-				editablePhoto[i,j,k] = ((editablePhoto[i,j,k])/255)
-			rNew=editablePhoto[i,j,0]
-			gNew=editablePhoto[i,j,1]
-			bNew=editablePhoto[i,j,2]
+# Converting Processed Array to Image
+def array_to_image(image_array, out_file_name):
+    rgb_array = np.array(image_array, dtype='uint8')
+    img = Image.fromarray(rgb_array)
+    img.save(out_file_name)
 
-			tempArray=np.zeros((3),'float')
 
-			for k in range(0,3):
-				hsvArray[i,j,k]=colorsys.rgb_to_hsv(editablePhoto[i,j,0],editablePhoto[i,j,1],editablePhoto[i,j,2])[k]
+def main():
+    args = parse_args()
+    input_image = Image.open(args.input)
+    lms_array = convert_to_lms(input_image)
+    if args.sp:
+        lms_array_transformed = convert_to_protanopes(lms_array)
+    elif args.sd:
+        lms_array_transformed = convert_to_deuteranopes(lms_array)
+    else:  # args.st
+        lms_array_transformed = convert_to_tritanope(lms_array)
+    rgb_array_transformed = convert_to_rgb(lms_array_transformed)
+    array_to_image(rgb_array_transformed, args.output)
 
-			
-			greenRatio = (hsvArray[i,j,0] - (60/360))/gNew
-			blueRange = greenRatio*bNew
-			hsvArray[i,j,0] = 0.5 + blueRange
 
-			tempArray=np.zeros((3),'float')
-			for k in range(0,3):
-				tempArray[k]=hsvArray[i,j,k]
-			tempArray.tolist()
-			tempArray = (colorsys.hsv_to_rgb(tempArray[0],tempArray[1],tempArray[2]))
-
-			for k in range(0,3):
-				editablePhoto[i,j, k] = tempArray[k]*255
-
-	NormalPhoto = normalise(editablePhoto, sizeX, sizeY)
-	arrayToImage(NormalPhoto,sizeX,sizeY,saveAs)
-
-#Main function decides which function to call based on user choice in ArgParse
-def main(user_choice):
-	inputIm = Image.open(user_choice.input)
-	sizeX = inputIm.size[0]
-	sizeY = inputIm.size[1]
-
-	if simType != 4:
-		lmsPhoto = convertToLMS(inputIm,sizeX,sizeY)
-		if simType == 1:
-			simPhoto = ConvertToProtanopes(lmsPhoto,sizeX,sizeY)
-		elif simType == 2:
-			simPhoto = ConvertToDeuteranopes(lmsPhoto,sizeX,sizeY)
-		elif simType == 3:
-			simPhoto = ConvertToTritanope(lmsPhoto,sizeX,sizeY)
-
-		rgbPhoto = convertToRGB(simPhoto,sizeX,sizeY)
-		arrayToImage(rgbPhoto,sizeX,sizeY,user_choice.output)
-	elif simType == 4:
-		correct(inputIm,sizeX,sizeY,user_choice.output)
-
-#Fetching ArgParse Data
 if __name__ == '__main__':
-	user_choice = parse_args()
-	if user_choice.sp == False and user_choice.sd == False and user_choice.st == False and user_choice.correct == False:
-		print("\nNo Choice Input. Try --help for help\n")
-		sys.exit()
-	elif user_choice.sp == True:
-		simType=1
-	elif user_choice.sd == True:
-		simType=2
-	elif user_choice.st == True:
-		simType=3
-	elif user_choice.correct == True:
-		simType=4
-
-	if not user_choice.input or not user_choice.output:
-		print("Enter both Input and Output Image paths. Try --help for help")
-		sys.exit()
-	main(user_choice)
+    main()
